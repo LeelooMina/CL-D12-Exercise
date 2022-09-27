d4c25ddb8ddcf7dd4e87dae760ced523dd9fcc0b
Crystal Campbell <blankpage@gmail.com>
2022-09-26 19:57:22 -0500

    ADD: Exercise 8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { getCurrencySymbol } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,6 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title:string = 'header';
-  isShowContent: boolean = true;
+  myColor: string = "green";
 
 }
